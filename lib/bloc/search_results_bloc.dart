ce7d756cdbd78420b6d257319ae03f73d0305461
Angel D <angeldeniz11235@gmail.com>
2021-06-28 18:02:56 -0700

    end of branch - not successdful

--- lib/bloc/search_results_bloc.dart
+++ lib/bloc/search_results_bloc.dart
@@ -1,16 +1,23 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_dev_tutorial2/main.dart';
 
-class SearchResBlock extends Bloc<String, List<Bar>> {
-  SearchResBlock() : super(List<Bar>.empty());
-
-  List<Bar> get initialState => List<Bar>.empty();
-
-  @override
-  Stream<List<Bar>> mapEventToState(String query) async* {
-    yield state
-        .where((bar) => bar.name.toLowerCase().contains(query.toLowerCase()))
-        .toList();
-  }
+class BarListAndQuery {
+  List<Bar> list;
+  String query;
+  BarListAndQuery(this.list, this.query);
 }
+
+// class SearchResBloc extends Bloc<, List<Bar>> {
+//   SearchResBloc() : super(List<Bar>.empty());
+
+//   List<Bar> get initialState => List<Bar>.empty();
+
+//   @override
+//   Stream<List<Bar>> mapEventToState(Bar list) async* {
+//     yield state;
+//     // yield state
+//     //     .where((bar) => bar.name.toLowerCase().contains(query.toLowerCase()))
+//     //     .toList();
+//   }
+// }
+//
