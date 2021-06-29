cd8439468b2040169a27ce18653e444d89c4b9a4
Angel D <angeldeniz11235@gmail.com>
2021-06-29 07:35:32 -0700

    no errors in favorites_bloc

--- lib/bloc/favorites_bloc.dart
+++ lib/bloc/favorites_bloc.dart
@@ -1,24 +1,30 @@
 import 'dart:async';
 
 class FavoritesBloc {
+  //init and get int StreamContoller
+  final _indexStreamController = StreamController<int>();
+  StreamSink<int> get indexSink => _indexStreamController.sink;
+
+  //expose int stream
+  Stream<int> get indexStream => _indexStreamController.stream;
+
   //init and get List<bool> StreamContoller
-  final _listStreamController = StreamController<List<bool>>();
-  StreamSink<List<bool>> get listSink => _listStreamController.sink;
+  final _favListStreamController = StreamController<List<bool>>();
+  StreamSink<List<bool>> get favListSink => _favListStreamController.sink;
 
   //expose List<bool> stream
-  Stream<List<bool>> get listStream => _listStreamController.stream;
+  Stream<List<bool>> get favListStream => _favListStreamController.stream;
 
-  SearchResBloc(List<bool> list) {
-    _queryStreamController.stream.listen((event) {
-      List<bool> filteredList =
-          list.where((x) => x.name.toLowerCase().contains(event)).toList();
-
-      listSink.add(filteredList);
+  FavoritesBloc(List<bool> list) {
+    List<bool> _isFavorite = List.filled(list.length, false);
+    _indexStreamController.stream.listen((index) {
+      _isFavorite[index] = !_isFavorite[index];
+      favListSink.add(_isFavorite);
     });
   }
 
   dispose() {
-    _queryStreamController.close();
-    _listStreamController.close();
+    _indexStreamController.close();
+    _favListStreamController.close();
   }
 }
